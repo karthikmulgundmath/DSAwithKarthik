8091fbbc3284fd28a4762bf803bd3babebe06ed5
Karthik <karthiksrk40@gmail.com>
2023-02-19 19:54:07 +0530

    Learning DSA Again This time wont stop

---
+++ index.js
@@ -0,0 +1,66 @@
+// Starting DSA Again
+
+//O(n)
+
+// function logItems(n) {
+//     for (let i = 0; i < n; i++) {
+//         console.log(i)
+//     }
+// }
+// logItems(10)
+
+
+//O(n2)
+
+// function logItems(n) {
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < n; j++) {
+//             console.log(i, j)
+//         }
+//     }
+// }
+// logItems(10)
+
+
+//O(n3):- O(n2 only)
+
+// function logItems(n) {
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < n; j++) {
+//             for (let k = 0; k < n; k++) {
+//                 console.log(i, j, k)
+//             }
+//         }
+//     }
+// }
+// logItems(10)
+
+//O(1)
+
+// function addItems(n) {
+//     return n + n
+// }
+// console.log(addItems(1));
+
+//class and constructor
+
+// class Cookie {
+//     constructor(color) {
+//         this.color = color
+//     }
+//     getColor() {
+//         return this.color
+//     }
+//     setColor(color) {
+//         this.color = color
+//     }
+// }
+
+// let cookieOne = new Cookie('green');
+// console.log(cookieOne);
+
+// let colorOfCookie = cookieOne.getColor();
+// console.log(colorOfCookie);
+
+// cookieOne.setColor('yellow')
+// console.log(cookieOne);
